44cb5fb19f010e8f292ffe9d6c6c2d40beac85e4
VismayaVinod47 <vismayanv2004@gmail.com>
2025-12-24 15:13:51 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,14 @@
+#directory structure
+mkdir -p src
+mkdir -p research
+#file structure
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch app.py
+touch research/triala.ipynb
+touch requirements.txt
+
+
+echo "Project structure created successfully."
